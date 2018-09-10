d47fc7090473c919561aba1e2ab228293dec8638
cvogit <cvogit@gmail.com>
2018-09-09 19:38:59 -0700

    Added verifiation page

--- src/components/Verification.js
+++ src/components/Verification.js
@@ -1,23 +1,40 @@
 import React, { Component } from 'react';
 import axios  from 'axios';
 
+import logo2 from '../images/logo-2.png';
+
 const SERVER_ADDRESS = process.env.REACT_APP_TEST_VAR;
 
 class Verification extends Component {
+	constructor(props) {
+		super(props);
+    this.state = {
+      message: ''
+   	};
+	}
 
-	handleVerification = () => {
-    axios.post(SERVER_ADDRESS+this.props.match.url)
+  componentDidMount() {
+  	axios.post(SERVER_ADDRESS+this.props.match.url)
     .then((response) => {
+    	this.setState({
+    		message: 'success'
+    	})
     })
     .catch((error) => {
       console.log(error);
     });
-  };
+  }
 
 	render() {
+		var message = "Please wait for verification.";
+
+		if(this.state.message === 'success') {
+			message = "Verification successful, enjoy DisoDat."
+		}
     return (
       <div className="verification-container">
-      	
+      	<img className="logo-2" alt="logo-2" src={logo2} />
+      	<h4>{message}</h4>
       </div>
     );
   }
